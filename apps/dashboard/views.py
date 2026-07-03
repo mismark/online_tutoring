3cdf2c501bccacbe0dcc2779ba84f8cba1bef117
YourName <your@email.com>
2026-07-03 15:22:51 +0300

    create a dashboard template files

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -1,4 +1,16 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+
+@login_required
 def home(request):
-    return render(request, "dashboard/home.html")
+
+    context = {
+        "user": request.user,
+    }
+
+    return render(
+        request,
+        "dashboard/home.html",
+        context
+    )
